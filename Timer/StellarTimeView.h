15a97228f5079a8c8761cb0d9750432edae8f636
Tongle <Tongle@sansi.com>
2017-03-15 13:21:21 +0800

    first commit

---
+++ Timer/StellarTimeView.h
@@ -0,0 +1,16 @@
+//
+//  StellarTimeView.h
+//  stellar-wifi
+//
+//  Created by tongle on 2017/3/9.
+//  Copyright © 2017年 qingpei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StellarTimeView : UIView
+@property (nonatomic,assign) CGFloat percent;
+@property (nonatomic,strong)UILabel *progressView;  //  进度文字
+
+
+@end
